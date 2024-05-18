98b811853d38df74bdf10b6161f200711a41c7dd
Dominika Bomba <146015989+DominikaBomba@users.noreply.github.com>
2024-05-18 19:29:27 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+const API_KEY  = "40489592-8efeacaa8de91011aad87ca09&q";
+document.addEventListener('DOMContentLoaded', function() {
+    console.log(`Connecting using API key: ${API_KEY}`);
+    
+});
